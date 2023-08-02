f76f3ef6dc5729a0a9b4223a3bdb5effef7ba2a4
Anya <anyagoel6@gmail.com>
2023-08-02 09:30:43 -0400

    Initial Commit

---
+++ articles/articlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  articlesApp.swift
+//  articles
+//
+//  Created by scholar on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct articlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
